f1458c79dac6aa33c3e8d4ddab01ae2197984de3
demba75009 <b-demba@hotmail.fr>
2023-07-10 14:19:54 +0200

    Livraison

--- src/components/Commande/Commande.tsx
+++ src/components/Commande/Commande.tsx
@@ -6,13 +6,25 @@ export default function Commande () {
     let cart = useContext(CartContext || null)
 
     const [Cart, setCart] = useState(cart || [])
+    const [Adresse, setAdresse] = useState("")
+    const [Ville, setVille] = useState("")
+    const [CodePostal, setCodePostal] = useState("")
 
-    console.log(Cart);
     
 
-    function Commandez(){
+    function Commandez(e:event){
 
-        console.log("ok");
+        e.preventDefault()
+
+        const Livraison = {
+
+            Adresse,
+            Ville,
+            CodePostal
+
+        }
+
+        console.log(Livraison);
         
     }
 
@@ -45,7 +57,6 @@ export default function Commande () {
         <>
         
             <h1 className="my-20"> Votre Commande :  </h1>
-            { Cart.length > 0 ? (
                 <>
             <table className="my-40 container mx-auto border-separate border-collapse border border-current	-400" >
 
@@ -55,7 +66,7 @@ export default function Commande () {
                             <th className="border border-current	-300" scope="row">Name</th>
                             <th className="border border-current	-300">Prix</th>
                             <th className="border border-current	-300">quantity</th>
-                            <th>Action</th>
+                          
               </tr>
               </thead>                    
 
@@ -69,18 +80,53 @@ export default function Commande () {
                             <td className="border border-current	-300">{c.Nom}</td>
                             <td className="border border-current	-300" >{c.Prix} $</td>
                             <td className="border border-current	-300" >{c.Stock}</td>
-                            <td onClick={()=>SupprimerPanier(c.idCart)} className="border border-current	-300 text-red-500" >Supprimmer</td>
+                           
                         </tr>
 
                    </tbody>       
 
             )) }
             </table>  
-                <button onClick={Commandez}  className="GetStarted mr-4">Passez au Mode de livraison</button>
+
+              <h2> Livraison (sous 2j ouvré) </h2>
+
+             <form className={` bg-white shadow-md rounded px-8 pt-6 my-10 pb-8 mb-4`}>
+
+
+                    <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="Adresse">
+                        Adresse
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Adresse" type="text"  name="Adresse"  placeholder="Adresse"  onKeyUp={(e)=>{setAdresse(e.currentTarget.value)}}/>
+                    </div>
+
+
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="Ville">
+                        Ville
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="Ville" type="text"  name="Ville" placeholder="Ville" onKeyUp={(e)=>{setVille(e.currentTarget.value)}}/>
+
+                  
+                    <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="Nom">
+                        Code postal
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Codepostal" type="text" name="Codepostal"  placeholder="Code postal" onKeyUp={(e)=>{setCodePostal(e.currentTarget.value)}}/>
+
+                    {/* <p className="text-red-500">{ErrorPrix}</p> */}
+                    </div>
+
+            
+
+                    
+
+                <button onClick={Commandez}  className="GetStarted mr-4">Passez au paiement</button>
+            
+            </form>
             </>  
 
-          ) : <h1 className="my-20 text-center"> Votre Panier est vide </h1>
-         }
+          
+         
 
             </>
 
